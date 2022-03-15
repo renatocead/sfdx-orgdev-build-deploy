9d64f4e82f29218897f5fe978589923dc2034dc1
Renato Ceadareanu <rceadareanu@salesforce.com>
2022-03-15 14:11:16 -0300

    Teste

--- dist/index.js
+++ dist/index.js
@@ -13571,9 +13571,9 @@ let login = function (cert, login){
 let convertion = function(deploy){
     core.info("=== converting ===");
     core.info("=== creating dir ===");
-    execCommand.run('mkdir ready2Deploy');
+    execCommand.run('mkdir', ['ready2Deploy']);
     core.info("=== run source convert ===");
-    execCommand.run('sfdx force:source:convert -d ready2Deploy')
+    execCommand.run('sfdx', ['force:source:convert -d ready2Deploy'])
     core.info("=== converted ===");
 };
 
