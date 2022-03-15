03bc430ea47c3e5bfd372542f6860f494549a2d3
Renato de Carvalho Ceadareanu <renatoceadareanu@gmail.com>
2022-03-15 14:26:16 -0300

    Update sfdx.js

--- src/utils/sfdx.js
+++ src/utils/sfdx.js
@@ -54,9 +54,9 @@ let login = function (cert, login){
 let convertion = function(deploy){
     core.info("=== converting ===");
     core.info("=== creating dir ===");
-    execCommand.run('mkdir', ['/usr/ready2Deploy']);
+    execCommand.run('mkdir', ['sudo /opt/ready2Deploy']);
     core.info("=== run source convert ===");
-    execCommand.run('sfdx', ['force:source:convert -d /usr/ready2Deploy'])
+    execCommand.run('sfdx', ['force:source:convert -d /opt/ready2Deploy'])
     core.info("=== converted ===");
 };
 
