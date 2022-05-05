685fc7c76562568e52746f380a4aa6e4814c50db
Renato de Carvalho Ceadareanu <renatoceadareanu@gmail.com>
2022-05-05 07:56:11 -0300

    Update sfdx.js

--- src/utils/sfdx.js
+++ src/utils/sfdx.js
@@ -62,7 +62,7 @@ let convertion = function(deploy){
     core.info("=== full converted ===");
 
     core.info("=== running GIT Delta ===");
-    execCommand.run('sfdx', ['sgd:source:delta --to "HEAD" --from "Develop" --output /opt/ready2Deploy']);
+    execCommand.run('sfdx', ['sgd:source:delta', '--to', '"HEAD"', '--from', '"Develop"', '--output', '/opt/ready2Deploy']);
     execCommand.run('sh', ['-c', 'ls /opt/ready2Deploy']);
     execCommand.run('sh', ['-c', 'mv /opt/ready2Deploy/destructiveChanges/ /opt/ready2Deploy/package']);
 };
