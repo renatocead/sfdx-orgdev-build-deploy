108902d4755c0fb3f19fac5a637e75f5d52214d8
Renato de Carvalho Ceadareanu <renatoceadareanu@gmail.com>
2022-05-09 07:38:17 -0300

    Update sfdx.js

--- src/utils/sfdx.js
+++ src/utils/sfdx.js
@@ -59,7 +59,7 @@ let convertion = function(deploy){
     execCommand.run('sfdx', ['force:source:convert', '-d', '/opt/ready2Deploy'])
     execCommand.run('sh', ['-c', 'cp -R /opt/ready2Deploy ./devops/destructive']);
     core.info("=== Output dir destructive ===");
-    execCommand.run('sh', ['-c', 'ls -R /devops/destructive']);
+    execCommand.run('sh', ['-c', 'ls -R ./devops/destructive']);
     core.info("=== full converted ===");
 
     //core.info("=== running GIT Delta ===");
